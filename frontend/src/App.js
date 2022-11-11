f64a8cd54579984750542c841d518162349dbb2e
PradipKafleDev <pradipkafle074@gmail.com>
2022-11-11 19:51:15 +1100

    homepage updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
-import Categories from "./components/Categories";
-import Footer from "./components/Footer";
-import Newsletter from "./components/Newsletter";
-import Products from "./components/Products";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
       <Home />
-      <Categories />
-      <Products />
-      <Newsletter />
-      <Footer />
     </div>
   );
 };
