ad3c6131c8ed1a4e1cdab92de528841da2104e46
PradipKafleDev <pradipkafle074@gmail.com>
2022-11-12 14:50:56 +1100

    responsive added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,16 +9,20 @@ import {
 } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   display: flex;
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const Left = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
+  padding: 20px;
 `;
+
 const Logo = styled.h1``;
 
 const Desc = styled.p`
@@ -33,12 +37,18 @@ const SocialIcon = styled.div`
   width: 40px;
   height: 40px;
   border-radius: 50%;
+  color: white;
   background-color: #${props => props.color};
   display: flex;
   align-items: center;
   justify-content: center;
-  color: white;
   margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ display: "none" })}
 `;
 
 const Title = styled.h3`
@@ -52,23 +62,24 @@ const List = styled.ul`
   display: flex;
   flex-wrap: wrap;
 `;
+
 const ListItem = styled.li`
   width: 50%;
   margin-bottom: 10px;
 `;
 
-const Center = styled.div`
-  flex: 1;
-  padding: 20px;
-`;
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })}
 `;
+
 const ContactItem = styled.div`
   margin-bottom: 20px;
   display: flex;
   align-items: center;
 `;
+
 const Payment = styled.img`
   width: 50%;
 `;
@@ -76,19 +87,18 @@ const Payment = styled.img`
 const Footer = () => {
   return (
     <Container>
-      {/* left container */}
       <Left>
-        <Logo>KAFLE.</Logo>
+        <Logo>LAMA.</Logo>
         <Desc>
-          There are many varitaions of passages of Lorem ipsum, dolor sit amet
-          consectetur adipisicing elit. Tempore rem suscipit ratione possimus
-          reiciendis iure cumque necessitatibus ducimus aut?
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don’t look even slightly believable.
         </Desc>
         <SocialContainer>
           <SocialIcon color="3B5999">
             <Facebook />
           </SocialIcon>
-          <SocialIcon color="E44057">
+          <SocialIcon color="E4405F">
             <Instagram />
           </SocialIcon>
           <SocialIcon color="55ACEE">
@@ -99,11 +109,8 @@ const Footer = () => {
           </SocialIcon>
         </SocialContainer>
       </Left>
-
-      {/* Center container */}
       <Center>
         <Title>Useful Links</Title>
-
         <List>
           <ListItem>Home</ListItem>
           <ListItem>Cart</ListItem>
@@ -113,23 +120,21 @@ const Footer = () => {
           <ListItem>My Account</ListItem>
           <ListItem>Order Tracking</ListItem>
           <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
           <ListItem>Terms</ListItem>
         </List>
       </Center>
-
-      {/* Right */}
       <Right>
         <Title>Contact</Title>
         <ContactItem>
-          <Room /> 96 Mapleton Avenue, Canberra
+          <Room style={{ marginRight: "10px" }} /> 622 Dixie Path , South
+          Tobinchester 98336
         </ContactItem>
         <ContactItem>
-          <Phone /> +617888996522
+          <Phone style={{ marginRight: "10px" }} /> +1 234 56 78
         </ContactItem>
         <ContactItem>
-          {" "}
-          <MailOutline />
-          contact@kafle.com
+          <MailOutline style={{ marginRight: "10px" }} /> contact@lama.dev
         </ContactItem>
         <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
       </Right>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 import { Search, ShoppingCartOutlined } from "@mui/icons-material";
 import Badge from "@mui/material/Badge";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 90px;
+  ${mobile({ height: "50px" })}
 `;
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
 `;
 const Left = styled.div`
   display: flex;
@@ -18,6 +21,7 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 12px;
   cursor: pointer;
+  ${mobile({ flex: 2, display: "none" })}
 `;
 const SearchContainer = styled.div`
   border: 1px solid lightgray;
@@ -29,6 +33,7 @@ const SearchContainer = styled.div`
 const Input = styled.input`
   outline: none;
   border: none;
+  ${mobile({ width: "50px" })}
 `;
 
 const Center = styled.div`
@@ -37,17 +42,20 @@ const Center = styled.div`
 `;
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
 `;
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({ justifyContent: "center" })}
 `;
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
 
 const Navbar = () => {
@@ -58,7 +66,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
+            <Input placeholder="Search" />
             <Search style={{ color: "gray", fontSize: "16px" }} />
           </SearchContainer>
         </Left>
